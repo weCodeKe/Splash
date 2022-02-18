cce4b6549e1cc532fd57cfd9e810648c007ef851
johnwaithira <waithirajon@gmail.com>
2022-02-18 16:20:37 +0300

    Initial commit

---
+++ app/src/main/java/com/waithirajon/splash/Home.kt
@@ -0,0 +1,13 @@
+package com.waithirajon.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        supportActionBar!!.hide()
+    }
+}
